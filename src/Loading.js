ac14b892e01935ff17b50ee8aa0e982196bf00dc
Vikash Kumar <vikkuvikash79097@gmail.com>
2023-03-19 23:44:16 +0530

    first commit

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import { ColorRing } from "react-loader-spinner";
 
 const Loading = () => {
   return (
     <div className="loading">
       <h1>loading...</h1>
+      <ColorRing
+        visible={true}
+        height="180"
+        width="180"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
     </div>
   );
 };
--- src/Tour.js
+++ src/Tour.js
@@ -17,7 +17,7 @@ const Tour = ({ id, image, info, price, name, removeTour }) => {
         <footer>
           <div className="tour-info">
             <h4>{name}</h4>
-            <h4 className="tour-price">{price}</h4>
+            <h4 className="tour-price">₹{price}</h4>
           </div>
           <p>
             {readMore ? info : `${info.substring(0, 200)}...`}
